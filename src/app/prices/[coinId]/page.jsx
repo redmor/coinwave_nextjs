4a9171c09e1c58fb75229c251745dbee9189e31c
redmor <errabili.oussama@gmail.com>
2023-07-30 00:57:38 -0400

    Styling the single coin page

--- src/app/prices/[coinId]/page.jsx
+++ src/app/prices/[coinId]/page.jsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 
-let USDollar = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+let USDollar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
 });
 
-
 export default async function GetCoinData({ params: { coinId } }) {
-
-
   const res = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`, { next: { revalidate: 10 } });
 
   const data = await res.json();
 
-//   if (!Array.isArray(data)) {
-//     return <p>No data available.</p>;
-//   }
+  //   if (!Array.isArray(data)) {
+  //     return <p>No data available.</p>;
+  //   }
 
   return (
-    <article className="max-w-7xl mx-4 sm:mx-auto mb-12">
-      <Image src={data.image.large} alt={data.name} width={200} height={200} priority={true}/>
-      <h1 className="text-7xl">{USDollar.format(data.market_data.current_price.usd)}</h1>
-      <a href={data.links.homepage.slice(0, -2)}>{data.links.homepage}</a>
-      {<h1 key={data.id} dangerouslySetInnerHTML={{ __html: data.description.en }} />}
+    <article className="max-w-7xl mx-4 sm:mx-auto my-40 flex gap-10">
+      <div className="border border-purple-800 p-10 flex flex-col items-center justify-between min-h-[500px]">
+        <Image src={data.image.large} alt={data.name} width={200} height={200} priority={true} />
+        <p className="text-7xl">{USDollar.format(data.market_data.current_price.usd)}</p>
+        <a className="hover:text-gray-400" href={data.links.homepage.slice(0, -2)}>
+          {data.links.homepage}
+        </a>
+      </div>
+      <div className="border border-purple-800 p-10">{data.description.en ? <h1 key={data.id} dangerouslySetInnerHTML={{ __html: data.description.en }} /> : <h1 className="text-5xl">Description not available</h1>}</div>
     </article>
   );
 }
