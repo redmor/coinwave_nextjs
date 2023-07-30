8ad4d0320822472bee81895aaefbe2b88715256d
redmor <errabili.oussama@gmail.com>
2023-07-30 01:14:00 -0400

    Added the About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,14 @@
 function About() {
   return (
-    <article className="max-w-7xl mx-4 xl:mx-auto my-40">
-      <div>About Page</div>
-      <section className=" bg-gradient-to-b from-slate-700 p-3 rounded-t-3xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam error aperiam consectetur voluptatibus corporis facere, debitis dignissimos iusto! Eius, reprehenderit alias! Cumque dolores quasi earum ducimus id odit similique tempore provident ut, optio sunt modi unde! Maiores voluptatum expedita natus ut eveniet distinctio temporibus numquam esse aliquid aut doloribus, laboriosam, odio libero ab? Facere non assumenda eaque maiores error nostrum amet autem eos cupiditate dolor quos placeat minima quasi, quas recusandae ducimus, quae iusto fuga aspernatur laborum. Voluptatibus omnis
-          cum, veniam similique quos quod beatae eos eius amet ratione ex et delectus? Ratione delectus accusamus praesentium ullam at. Voluptatem, corrupti!
-        </section>
+    <article className="max-w-4xl mx-4 xl:mx-auto my-40">
+      <section className=" bg-gradient-to-b from-slate-700 px-10 rounded-t-3xl ">
+        <h1 className="text-4xl text-center p-5">Welcome to Coinwave.us!</h1> <br />
+        <p>At CoinWave.us, we are dedicated to providing our users with the latest and most comprehensive information about cryptocurrencies. Whether you&apos;re a curious enthusiast, or just getting started in the world of digital assets, our platform is designed to be your go-to resource for real-time prices and up-to-date news.</p> <br />
+        <p>Built from the ground up using cutting-edge technologies like React, Next.js, and Restful APIs, CoinWave.us offers a seamless and user-friendly experience.</p><br />
+        <h1 className="text-3xl">Our Mission:</h1> <br />
+        <p>At CoinWave.us, our mission is to empower individuals to navigate the dynamic world of cryptocurrencies confidently. We believe that democratizing access to accurate and reliable information is the key to fostering a stronger, more informed crypto community. By providing a one-stop platform for prices, news, and educational resources, we hope to inspire curiosity, encourage learning, and facilitate responsible decision-making in the crypto space.</p>
+      </section>
     </article>
-  )
+  );
 }
-export default About
+export default About;
